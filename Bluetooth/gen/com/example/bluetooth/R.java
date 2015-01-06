afd9967544e4cd644d6986b88372f5b103f852d0
alex-ta <alexander.taubert@hs-furtwangen.de>
2015-01-06 22:40:21 +0100

    checked fuel input

--- Bluetooth/gen/com/example/bluetooth/R.java
+++ Bluetooth/gen/com/example/bluetooth/R.java
@@ -1091,13 +1091,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int b_listView=0x7f050040;
         public static final int b_refresh_btn=0x7f05003f;
         public static final int beginning=0x7f050011;
         public static final int button_icheck=0x7f05003e;
+        public static final int button_obdonoff=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1199,7 +1200,8 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int input_check=0x7f030018;
         public static final int select_bluetooth=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int startbutton=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1248,7 +1250,9 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int offobd=0x7f0a0013;
         public static final int ok=0x7f0a0011;
+        public static final int onobd=0x7f0a0012;
         public static final int refresh=0x7f0a0010;
     }
     public static final class style {
